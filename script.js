aeaa9ce092ae41aa1f0d85c265b960b0726759a6
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2024-05-22 22:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //your JS code here. If required.
-let num1 = prompt("enter number 1");
-let num2 = prompt("enter number 2");
-alert(num1 === num2? "true": "false");
+// let num1 = prompt("enter number 1");
+// let num2 = prompt("enter number 2");
+// alert(num1 === num2? "true": "false");
+
+let num1 = prompt("num1");
+let num2 = prompt("num2");
+alert(num1==num2);
